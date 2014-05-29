84983ac700f3f289360e3882497aa53b6a3d7554
bigcmel <myqqis306584800@163.com>
2014-05-29 22:23:54 +0800

    2014.5.29

---
+++ kernel/main.c
@@ -0,0 +1,14 @@
+#include "proto.h"
+
+
+BYTE* __main()
+{
+
+  /* 变量声明 */
+
+  unsigned int i;
+
+
+  LCD_ClearScr( 0x909090 ); // 示意进入了内核
+
+};
---
+++ kernel/proto.h
@@ -0,0 +1,33 @@
+/*
+proto.h: 该文件包含了供源文件调用的函数声明，有了这些声明，源文件才能调用这些函数
+*/
+
+#ifndef __PROTO_H__
+#define __PROTO_H__
+
+#include "global.h"
+
+
+/* nand_flash.c:
+ 注意这里的函数（写，读，擦除，标志坏块）在运行之前全都不检查是否是坏块，
+ 而且即使操作不成功，也不会标志为坏块，
+ 不管是检测坏块还是标志坏块都要在函数外部使用 NF_MarkBadBlock 和 NF_IsBadBlock 函数完成
+*/
+extern void NF_init();
+extern void NF_GetBlockPageInfo(unsigned int* nf_blocknum, unsigned int* nf_pagepblock,
+				unsigned int* nf_mainsize, unsigned int* nf_sparesize);
+extern WORD NF_CheckId(); // 获取厂商ID和设备ID
+extern unsigned int NF_WritePage(unsigned int block,unsigned int page,BYTE* buffer);
+extern unsigned int NF_ReadPage(unsigned int block,unsigned int page,BYTE* buffer);
+extern unsigned int NF_EraseBlock(unsigned int block); // 擦除一个块
+extern unsigned int NF_MarkBadBlock(unsigned int block); // 把一个块标记为坏块
+extern unsigned int NF_IsBadBlock(unsigned int block); // 判断一个块是不是坏块
+
+/* lcd.c: */
+extern void LCD_Init();
+extern void LCD_EnvidOnOff(int onoff);
+extern void LCD_PutPixel(WORD x, WORD y, WORD color);
+extern void LCD_ClearScr(WORD color);
+
+
+#endif
